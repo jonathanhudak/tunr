b8dc652ee0287a70d0b12a43ac0b1783ec5e7b22
Jonathan Hudak <hudak.jonathan@gmail.com>
2023-04-11 20:13:54 -0700

    Add permalink to tuning page

--- src/routes/tuning/[tuning]/+page.svelte
+++ src/routes/tuning/[tuning]/+page.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
 	export let data;
 	import GuitarTuner from '$lib/Tunr.svelte';
+	import { onMount } from 'svelte';
+
+	let url = ``;
+
+	onMount(() => (url = window.location.href));
 </script>
+
+Permalink:
+<a href={url}>{url}</a>
 
 <GuitarTuner tuning={data.tuning} />
